0a296560a9beeb73b0b8afc57d886fe06332bba5
Ernesta <ernesta.pesliakaite@gmail.com>
2022-07-10 15:57:13 +0300

    pirmas su main

---
+++ JS/app.js
@@ -0,0 +1,624 @@
+//NAV MODAL
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
+//END NAV MODAL
+
+//NEWS AND EVENT/////////////
+
+const activity = document.querySelector(".activity");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+activity.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});  
+
+//CALENDAR
+
+!function() {
+
+  var today = moment();
+
+  function Calendar(selector, events) {
+    this.el = document.querySelector(selector);
+    this.events = events;
+    this.current = moment().date(1);
+    this.draw();
+    var current = document.querySelector('.today');
+    if(current) {
+      var self = this;
+      window.setTimeout(function() {
+        self.openDay(current);
+      }, 500);
+    }
+  }
+
+  Calendar.prototype.draw = function() {
+    //Create Header
+    this.drawHeader();
+
+    //Draw Month
+    this.drawMonth();
+
+    this.drawLegend();
+  }
+
+  Calendar.prototype.drawHeader = function() {
+    var self = this;
+    if(!this.header) {
+      //Create the header elements
+      this.header = createElement('div', 'header');
+      this.header.className = 'header';
+
+      this.title = createElement('h1');
+
+      var right = createElement('div', 'right');
+      right.addEventListener('click', function() { self.nextMonth(); });
+
+      var left = createElement('div', 'left');
+      left.addEventListener('click', function() { self.prevMonth(); });
+
+      //Append the Elements
+      this.header.appendChild(this.title); 
+      this.header.appendChild(right);
+      this.header.appendChild(left);
+      this.el.appendChild(this.header);
+    }
+
+    this.title.innerHTML = this.current.format('MMMM YYYY');
+  }
+
+  Calendar.prototype.drawMonth = function() {
+    var self = this;
+    
+    this.events.forEach(function(ev) {
+     ev.date = self.current.clone().date(Math.random() * (29 - 1) + 1);
+    });
+    
+    
+    if(this.month) {
+      this.oldMonth = this.month;
+      this.oldMonth.className = 'month out ' + (self.next ? 'next' : 'prev');
+      this.oldMonth.addEventListener('webkitAnimationEnd', function() {
+        self.oldMonth.parentNode.removeChild(self.oldMonth);
+        self.month = createElement('div', 'month');
+        self.backFill();
+        self.currentMonth();
+        self.fowardFill();
+        self.el.appendChild(self.month);
+        window.setTimeout(function() {
+          self.month.className = 'month in ' + (self.next ? 'next' : 'prev');
+        }, 16);
+      });
+    } else {
+        this.month = createElement('div', 'month');
+        this.el.appendChild(this.month);
+        this.backFill();
+        this.currentMonth();
+        this.fowardFill();
+        this.month.className = 'month new';
+    }
+  }
+
+  Calendar.prototype.backFill = function() {
+    var clone = this.current.clone();
+    var dayOfWeek = clone.day();
+
+    if(!dayOfWeek) { return; }
+
+    clone.subtract('days', dayOfWeek+1);
+
+    for(var i = dayOfWeek; i > 0 ; i--) {
+      this.drawDay(clone.add('days', 1));
+    }
+  }
+
+  Calendar.prototype.fowardFill = function() {
+    var clone = this.current.clone().add('months', 1).subtract('days', 1);
+    var dayOfWeek = clone.day();
+
+    if(dayOfWeek === 6) { return; }
+
+    for(var i = dayOfWeek; i < 6 ; i++) {
+      this.drawDay(clone.add('days', 1));
+    }
+  }
+
+  Calendar.prototype.currentMonth = function() {
+    var clone = this.current.clone();
+
+    while(clone.month() === this.current.month()) {
+      this.drawDay(clone);
+      clone.add('days', 1);
+    }
+  }
+
+  Calendar.prototype.getWeek = function(day) {
+    if(!this.week || day.day() === 0) {
+      this.week = createElement('div', 'week');
+      this.month.appendChild(this.week);
+    }
+  }
+
+  Calendar.prototype.drawDay = function(day) {
+    var self = this;
+    this.getWeek(day);
+
+    //Outer Day
+    var outer = createElement('div', this.getDayClass(day));
+    outer.addEventListener('click', function() {
+      self.openDay(this);
+    });
+
+    //Day Name
+    var name = createElement('div', 'day-name', day.format('ddd'));
+
+    //Day Number
+    var number = createElement('div', 'day-number', day.format('DD'));
+
+
+    //Events
+    var events = createElement('div', 'day-events');
+    this.drawEvents(day, events);
+
+    outer.appendChild(name);
+    outer.appendChild(number);
+    outer.appendChild(events);
+    this.week.appendChild(outer);
+  }
+
+  Calendar.prototype.drawEvents = function(day, element) {
+    if(day.month() === this.current.month()) {
+      var todaysEvents = this.events.reduce(function(memo, ev) {
+        if(ev.date.isSame(day, 'day')) {
+          memo.push(ev);
+        }
+        return memo;
+      }, []);
+
+      todaysEvents.forEach(function(ev) {
+        var evSpan = createElement('span', ev.color);
+        element.appendChild(evSpan);
+      });
+    }
+  }
+
+  Calendar.prototype.getDayClass = function(day) {
+    classes = ['day'];
+    if(day.month() !== this.current.month()) {
+      classes.push('other');
+    } else if (today.isSame(day, 'day')) {
+      classes.push('today');
+    }
+    return classes.join(' ');
+  }
+
+  Calendar.prototype.openDay = function(el) {
+    var details, arrow;
+    var dayNumber = +el.querySelectorAll('.day-number')[0].innerText || +el.querySelectorAll('.day-number')[0].textContent;
+    var day = this.current.clone().date(dayNumber);
+
+    var currentOpened = document.querySelector('.details');
+
+    //Check to see if there is an open detais box on the current row
+    if(currentOpened && currentOpened.parentNode === el.parentNode) {
+      details = currentOpened;
+      arrow = document.querySelector('.arrow');
+    } else {
+      //Close the open events on differnt week row
+      //currentOpened && currentOpened.parentNode.removeChild(currentOpened);
+      if(currentOpened) {
+        currentOpened.addEventListener('webkitAnimationEnd', function() {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('oanimationend', function() {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('msAnimationEnd', function() {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('animationend', function() {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.className = 'details out';
+      }
+
+      //Create the Details Container
+      details = createElement('div', 'details in');
+
+      //Create the arrow
+      var arrow = createElement('div', 'arrow');
+
+      //Create the event wrapper
+
+      details.appendChild(arrow);
+      el.parentNode.appendChild(details);
+    }
+
+    var todaysEvents = this.events.reduce(function(memo, ev) {
+      if(ev.date.isSame(day, 'day')) {
+        memo.push(ev);
+      }
+      return memo;
+    }, []);
+
+    this.renderEvents(todaysEvents, details);
+
+    arrow.style.left = el.offsetLeft - el.parentNode.offsetLeft + 27 + 'px';
+  }
+
+  Calendar.prototype.renderEvents = function(events, ele) {
+    //Remove any events in the current details element
+    var currentWrapper = ele.querySelector('.events');
+    var wrapper = createElement('div', 'events in' + (currentWrapper ? ' new' : ''));
+
+    events.forEach(function(ev) {
+      var div = createElement('div', 'event');
+      var square = createElement('div', 'event-category ' + ev.color);
+      var span = createElement('span', '', ev.eventName);
+
+      div.appendChild(square);
+      div.appendChild(span);
+      wrapper.appendChild(div);
+    });
+
+    if(!events.length) {
+      var div = createElement('div', 'event empty');
+      var span = createElement('span', '', 'No Events');
+
+      div.appendChild(span);
+      wrapper.appendChild(div);
+    }
+
+    if(currentWrapper) {
+      currentWrapper.className = 'events out';
+      currentWrapper.addEventListener('webkitAnimationEnd', function() {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('oanimationend', function() {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('msAnimationEnd', function() {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('animationend', function() {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+    } else {
+      ele.appendChild(wrapper);
+    }
+  }
+
+  Calendar.prototype.drawLegend = function() {
+    var legend = createElement('div', 'legend');
+    var calendars = this.events.map(function(e) {
+      return e.calendar + '|' + e.color;
+    }).reduce(function(memo, e) {
+      if(memo.indexOf(e) === -1) {
+        memo.push(e);
+      }
+      return memo;
+    }, []).forEach(function(e) {
+      var parts = e.split('|');
+      var entry = createElement('span', 'entry ' +  parts[1], parts[0]);
+      legend.appendChild(entry);
+    });
+    this.el.appendChild(legend);
+  }
+
+  Calendar.prototype.nextMonth = function() {
+    this.current.add('months', 1);
+    this.next = true;
+    this.draw();
+  }
+
+  Calendar.prototype.prevMonth = function() {
+    this.current.subtract('months', 1);
+    this.next = false;
+    this.draw();
+  }
+
+  window.Calendar = Calendar;
+
+  function createElement(tagName, className, innerText) {
+    var ele = document.createElement(tagName);
+    if(className) {
+      ele.className = className;
+    }
+    if(innerText) {
+      ele.innderText = ele.textContent = innerText;
+    }
+    return ele;
+  }
+}();
+
+!function() {
+  var data = [
+    // { eventName: 'Conference1', calendar: 'conference', color: 'bordo' },
+    // { eventName: 'Conference2', calendar: 'conference', color: 'bordo' },
+    // { eventName: 'Conference3', calendar: 'conference', color: 'bordo' },
+    // { eventName: 'Conference4', calendar: 'conference', color: 'bordo' },
+
+    { eventName: 'Conference1', calendar: 'conference', color: 'grey' },
+    { eventName: 'Conference2', calendar: 'conference', color: 'grey' },
+    { eventName: 'Conference3', calendar: 'conference', color: 'grey' },
+    { eventName: 'Conference4', calendar: 'conference', color: 'grey' },
+
+    { eventName: 'Seminar1', calendar: 'seminar', color: 'yellow' },
+    { eventName: 'Seminar2', calendar: 'seminar', color: 'yellow' },
+    { eventName: 'Seminar3', calendar: 'seminar', color: 'yellow' },
+    { eventName: 'Seminar4', calendar: 'seminar', color: 'yellow' },
+
+    { eventName: 'Event1', calendar: 'event', color: 'rose' },
+    { eventName: 'Event2', calendar: 'event', color: 'rose' },
+    { eventName: 'Event3', calendar: 'event', color: 'rose' },
+    { eventName: 'Event4', calendar: 'event', color: 'rose' }
+  ];
+
+  
+
+  function addDate(ev) {
+    
+  }
+
+  var calendar = new Calendar('#calendar', data);
+
+}();
+
+//END CALENDAR
+
+//END NEWS AND EVENT////////////////////
+
+// PARTNER WORDS
+
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "dean of university",
+      img:
+        "https://randomuser.me/api/portraits/women/45.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "director of institute of",
+      img:
+        "https://randomuser.me/api/portraits/women/46.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "director of institute of ",
+      img:
+        "https://randomuser.me/api/portraits/men/45.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "dean of department of",
+      img:
+        "https://randomuser.me/api/portraits/men/46.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+    {
+    id: 5,
+      name: "josh cold",
+      job: "dean of department of",
+      img:
+        "https://randomuser.me/api/portraits/men/47.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+  
+  //select items
+  const img = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
+  
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  
+  
+  //set starting item
+  let currentItem = 1;
+  
+  //load initial item
+  window.addEventListener("DOMContentLoaded", function(){
+  showPerson(currentItem);
+  
+  
+  });
+  //show person based on item
+  function showPerson(person){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent= item.job;
+    info.textContent = item.text;
+  }
+  
+  
+  //show next person
+  nextBtn.addEventListener("click", function (){
+    currentItem++;
+    if (currentItem > reviews.length-1){
+      currentItem = 0;
+    };
+    showPerson(currentItem);
+  });
+  
+  //show prev person
+  prevBtn.addEventListener("click", function (){
+    currentItem--;
+    if (currentItem < 0){
+      currentItem = reviews.length-1;
+    };
+    showPerson(currentItem);
+  });
+  
+  
+  //show random person
+  
+  randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    //console.log showPerson():void
+    showPerson();
+  })
+
+
+  // END PARTNERS WORD
+
+  //Statistics
+
+
+const items = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 5000);
+  // const increment = 1;
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = `${initialValue}+`;
+  }, 1);
+  // console.log(increaseCount);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
+
+  //END of Statistics
+
+
+
+
+
+  //===SEARCH===///
+  const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=10&format=json&origin=*&srsearch=';
+
+const formDOM = document.querySelector('.search-form');
+const inputDOM = document.querySelector('.search-form-input');
+const resultsDOM = document.querySelector('.search-results');
+
+formDOM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML =
+      '<div class="error"> please enter valid search term</div>';
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  resultsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultsDOM.innerHTML =
+        '<div class="error">no matching results. Please try again</div>';
+      return;
+    }
+    renderResults(results);
+  } catch (error) {
+    resultsDOM.innerHTML = '<div class="error"> there was an error...</div>';
+  }
+};
+
+const renderResults = (list) => {
+  const cardsList = list
+    .map((item) => {
+      const { title, snippet, pageid } = item;
+      return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>`;
+    })
+    .join('');
+  resultsDOM.innerHTML = `<div class="websearch-articles">
+          ${cardsList}
+        </div>`;
+};
+
+  //YEAR for FOOTER
+
+  const date = document.getElementById("date");
+  date.innerHTML = new Date().getFullYear();
+    //END Year for FOOTER
+
+
+  // FORM CONTACT US MESSAGE RECEIVE
+
+  function sendEmail(){
+    Email.send({
+      Host : "smtp.elasticemail.com", //1
+      Username :"emailfromdepweb@gmail.com",//2
+      Password :"**********35BEB",//3 vietoj šių trijų 1 2 3 eilučių įtarpti šį "/SecureToken:"bd0ecc52-908d-4706-a5f7-aeaef6b91a11",/" (iš smtpjs)
+      To : 'emailfromdepweb@gmail.com',
+      From : document.getElementById("contacts-email").value,
+      Subject : "New message from dep website",
+      Body : "Name: " + document.getElementById("contacts-name").value
+      +"<br> Email: " + document.getElementById("contacts-email").value
+      +"<br> Message: " + document.getElementById("contacts-message").value
+      }).then(
+        message => alert("Message Send Succesfully")
+      );
+};
+  // END FORM CONTACT US MESSAGE RECEIVE
